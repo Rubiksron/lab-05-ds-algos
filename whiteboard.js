07ef0825aab1eb0213fdb6e95387c3a870a164f0
Rubiksron <rubiksron@gmail.com>
2017-01-09 20:20:50 -0800

    removed an else statement and let the line of code stand alone like another 'if' statement. I added another if statement before the code returns the count total, where if the count total was === 0, then a console log would say so. I believe that this log proves my code sound.

--- whiteboard.js
+++ whiteboard.js
@@ -1,25 +1,26 @@
 'use strict';
 
 var arr = [1, 0, 3, 5, 3, 4, 5, 6, 2, 9];
+// var arr = [0, 0, 0, 0, 0, 0, 0, 0];
 
 function hasIncrementingSequence(arr) {
     var count = 0;
     var isIncrementing = false;
 
     for(var i = 1; i < arr.length; i++) {
-      if(arr[i] < arr[i-1]) {
+      console.log(arr[i-1] + ' compared to ' + arr[i]);
+      if(arr[i-1] > arr[i] ) {
         if(isIncrementing) {
           count += 1;
-          isIncrementing = false;
         }
-      }
-      else {
         isIncrementing = true;
-       }
+      }
      }
+
      if(isIncrementing) {
       count += 1;
       console.log(count);
+      if(count == 0) console.log('0');
       return count;
     }
 };
