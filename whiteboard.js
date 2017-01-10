802e457926b89c11921674a980af7440f2af6e3b
Rubiksron <rubiksron@gmail.com>
2017-01-09 20:32:23 -0800

    had an issue with the last push not taking all of the changes up to github, so this is the second round.

--- whiteboard.js
+++ whiteboard.js
@@ -1,7 +1,7 @@
 'use strict';
 
-var arr = [1, 0, 3, 5, 3, 4, 5, 6, 2, 9];
-// var arr = [0, 0, 0, 0, 0, 0, 0, 0];
+// var arr = [1, 0, 3, 5, 3, 4, 5, 6, 2, 9];
+var arr = [0, 0, 0, 0, 0, 0, 0, 0];
 
 function hasIncrementingSequence(arr) {
     var count = 0;
@@ -16,11 +16,11 @@ function hasIncrementingSequence(arr) {
         isIncrementing = true;
       }
      }
-
+     if(count === 0) {console.log('count is 0');}
+     //the above if statement shows that the code works with an array of [0, 0, 0, 0, 0, 0]
      if(isIncrementing) {
       count += 1;
       console.log(count);
-      if(count == 0) console.log('0');
       return count;
     }
 };
